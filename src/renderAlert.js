1c0ebad99e3b4946b0e20f285d02a4aedb450ef2
Anton Rehemae <rehemtoha@gmail.com>
2019-01-30 22:25:15 +0200

    redesign

--- src/renderAlert.js
+++ src/renderAlert.js
@@ -1,7 +1,7 @@
 export default (message) => {
   const div = document.getElementById('myAlert');
   const html = message.length === 0 ? '' : `
-  <div class="alert alert-dark" role="alert">
+  <div class="alert alert-dark justify-content-center mx-auto text-center shadow p-3 mb-5 bg-white rounded" style="width: 1200px; font-size: 32px" role="alert">
 ${message}
   </div>
   `;
--- src/renderList.js
+++ src/renderList.js
@@ -6,8 +6,8 @@ export default (list, handler) => {
   <ul>${list.map(({ title, description, items }) => {
     const id = _.uniqueId('titleFlow-');
     return `
-    <li class="list-group-item list-group-item-secondary col-4">
-      <h5 id="${id}">${title}</h5>
+    <li class="list-group-item list-group-item-secondary col-4 mx-auto shadow p-3 mb-5 bg-white rounded" style="width: 1200px;">
+      <p id="${id}" class="text-center display-4 ">${title}</p>
       <p>${description}</p>
     </li>
     <ul>
@@ -15,10 +15,10 @@ export default (list, handler) => {
       <li class="my-1">
         <div data-title=${id}>
           <!-- Button trigger modal -->
-          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+          <button type="button" class="btn btn-info" data-toggle="modal" data-target="#exampleModalCenter">
             description
           </button>
-          <a class="col-6" href="${linkArticle}">${titleArticle}</a>
+          <a class="col-6 text-white bg-secondary" href="${linkArticle}">${titleArticle}</a>
         </div>
       </li>`).join('')}
     </ul>
